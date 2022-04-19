8995c6f28b935f1e71e69ca078d92d051cca0d70
Booger-Man2020 <vatsevicho@gmail.com>
2022-04-19 20:51:06 +0300

    без теста

---
+++ src/main/java/Array.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Array {
+    public static void main(String[] args) throws Exception {
+        Integer i = 4;
+        List ints = (Arrays.asList(1, 2, 4, 4, 2, 3, 4, 1, 7));
+        if (!ints.contains(i)) {
+            throw new RuntimeException();
+        }
+        List ints2 = findArray(ints);
+        System.out.println(ints2);
+    }
+
+    public static List<Integer> findArray(List<Integer> list) {
+        return list.stream()
+                .dropWhile(value -> !value.equals(4))
+                .dropWhile(value -> value.equals(4))
+                .dropWhile(value -> !value.equals(4))
+                .filter(value -> !value.equals(4))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/Array2.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Array2 {
+    public static void main(String[] args) {
+        List ints = Collections.singletonList(cheackArray(Arrays.asList(1, 1, 3, 4, 1, 4, 4, 1, 4)));
+        System.out.println(ints);
+
+    }
+
+    public static boolean cheackArray (List<Integer> list) {
+        return list.stream()
+                .noneMatch(val -> !val.equals(1) && !val.equals(4));
+    }
+
+
+}
